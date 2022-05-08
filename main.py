6cb9fcbfbf34d8b2bb1276a5e47607efa0927084
RicentCB <ricardo.cb009@gmail.com>
2022-05-08 11:45:44 -0500

    [FEAT] Guardar modelo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import os
 import PIL
 import tensorflow as tf
@@ -81,13 +80,15 @@ data_augmentation = keras.Sequential(
 
 
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  data_augmentation,
+  layers.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Dropout(0.2),    # Regualuarizacion de abandonos
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
@@ -99,13 +100,19 @@ model.compile(optimizer='adam',
   metrics=['accuracy']
 )
 
+# Resumen del modelo
+model.summary()
+
 # ENTRANAR MODELO
-epochs=10
+epochs=15
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
+
+# Guardar modelo
+model.save('trainedModel.h5')
 
 # RESULTADOS DE ENTRENAMIENTO
 acc = history.history['accuracy']
@@ -129,23 +136,3 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
-
-
-
-# PREDICCION DE NUEVOS DATOS
-sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
-sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
-
-img = tf.keras.utils.load_img(
-    sunflower_path, target_size=(img_height, img_width)
-)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
-
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
